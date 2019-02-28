925fdac8695dd5df0d1643a4c8ff6aa5db698719
O F <teleclimber@gmail.com>
2019-02-28 14:58:13 -0600

    Make ds-host remotely debuggable

--- ds-dev-scripts/build-host.sh
+++ ds-dev-scripts/build-host.sh
@@ -1,3 +1,8 @@
 cd "$(dirname "$0")"/../ || exit
 
-GOOS=linux go build -o bin/ds-host cmd/ds-host/ds-host.go
+if [ "$1" == "debug" ]; then
+	echo Compiling for debug...
+	GOOS=linux go build -gcflags="all=-N -l" -o bin/ds-host cmd/ds-host/ds-host.go
+else
+	GOOS=linux go build -o bin/ds-host cmd/ds-host/ds-host.go
+fi
