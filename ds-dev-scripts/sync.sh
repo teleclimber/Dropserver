a57ba44c47058c60c846304be0b81a776f531101
O F <teleclimber@gmail.com>
2020-07-06 12:48:06 -0700

    Change sync script to match new dir

--- ds-dev-scripts/sync.sh
+++ ds-dev-scripts/sync.sh
@@ -1,5 +1,5 @@
 cd "$(dirname "$0")"/../ || exit
 
-rsync -rt --itemize-changes --exclude=.git/ --exclude=.DS_Store --exclude=/dist/ --exclude=frontend/node_modules/ . developer@10.211.55.9:~/go/src/github.com/teleclimber/DropServer/
+rsync -rt --itemize-changes --exclude=.git/ --exclude=.DS_Store --exclude=/dist/ --exclude=frontend/node_modules/ . developer@10.211.55.9:~/dropserver-code/
 
 #--modify-window=5
