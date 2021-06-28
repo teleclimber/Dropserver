dbd00217ac277e904a5e30033ecbb3e323d45ace
O F <teleclimber@gmail.com>
2021-06-28 15:32:21 -0700

    Fix build scripts for multi file cmd root folders

--- ds-dev-scripts/build-app-dev.sh
+++ ds-dev-scripts/build-app-dev.sh
@@ -2,7 +2,7 @@ cd "$(dirname "$0")"/../ || exit
 
 if [ "$1" == "debug" ]; then
 	echo Compiling app dev command for debug...
-	go build -gcflags="all=-N -l" -o dist/bin/ds-dev cmd/ds-dev/ds-dev.go
+	go build -gcflags="all=-N -l" -o dist/bin/ds-dev ./cmd/ds-dev
 else
 	go build -o dist/bin/ds-dev ./cmd/ds-dev
 fi
--- ds-dev-scripts/build-host.sh
+++ ds-dev-scripts/build-host.sh
@@ -2,7 +2,7 @@ cd "$(dirname "$0")"/../ || exit
 
 if [ "$1" == "debug" ]; then
 	echo Compiling for debug...
-	go build -gcflags="all=-N -l" -o dist/bin/ds-host cmd/ds-host/ds-host.go
+	go build -gcflags="all=-N -l" -o dist/bin/ds-host ./cmd/ds-host
 else
-	go build -o dist/bin/ds-host cmd/ds-host/ds-host.go
+	go build -o dist/bin/ds-host ./cmd/ds-host
 fi
