18356af9787078040eb065799d8c2b7fc38afb3f
O F <teleclimber@gmail.com>
2021-10-14 11:40:38 -0700

    chore: clean up config files
    Removed as much as possible from vscode settings.

--- vetur.config.js
+++ vetur.config.js
@@ -1,47 +1,20 @@
 // vetur.config.js
+// see https://vuejs.github.io/vetur/guide/setup.html#advanced
+
 /** @type {import('vls').VeturConfig} */
 module.exports = {
-	// **optional** default: `{}`
-	// override vscode settings
-	// Notice: It only affects the settings used by Vetur.
 	settings: {
-	  "vetur.useWorkspaceDependencies": false,
-	  "vetur.experimental.templateInterpolationService": true,
-	  "vetur.validation.template": true,
-	  "vetur.validation.interpolation": true
+		"vetur.useWorkspaceDependencies": false,
+		"vetur.experimental.templateInterpolationService": true,
+		"vetur.validation.template": true,
+		"vetur.validation.interpolation": true
 	},
-	// **optional** default: `[{ root: './' }]`
-	// support monorepos
 	projects: [
-	  {
-		// **required**
-		// Where is your project?
-		// It is relative to `vetur.config.js`.
-		root: './frontend-ds-dev',
-		// **optional** default: `'package.json'`
-		// Where is `package.json` in the project?
-		// We use it to determine the version of vue.
-		// It is relative to root property.
-		//package: './package.json',
-		// **optional**
-		// Where is TypeScript config file in the project?
-		// It is relative to root property.
-		//tsconfig: './tsconfig.json',
-		// **optional** default: `'./.vscode/vetur/snippets'`
-		// Where is vetur custom snippets folders?
-		//snippetFolder: './.vscode/vetur/snippets',
-		// **optional** default: `[]`
-		// Register globally Vue component glob.
-		// If you set it, you can get completion by that components.
-		// It is relative to root property.
-		// Notice: It won't actually do it. You need to use `require.context` or `Vue.component`
-		// globalComponents: [
-		//   './src/components/**/*.vue'
-		// ]
-	  },{
-		  root: './frontend-ds-host'
-	  }
-
+		{
+			root: './frontend-ds-dev',
+		},{
+			root: './frontend-ds-host'
+		}
 	]
-  }
+}
   
