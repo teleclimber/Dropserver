5c1bb054c7e85afc82409d83d93a7d6a53294900
O F <teleclimber@gmail.com>
2022-02-12 12:11:27 -0800

    fix(frontend-ds-dev): sandbox inspect button state

--- frontend-ds-dev/src/components/Sandbox.vue
+++ frontend-ds-dev/src/components/Sandbox.vue
@@ -1,10 +1,4 @@
 <template>
-<!-- sandbox feedback and some control:
-  - status (running/not)
-  - context: app, appspace, migrate
-  - kill
-  - inspect
-  -->
 	<div class="flex flex-col border-2 p-1 border-gray-500 rounded-lg text-sm">
 		<div class="mb-1 flex justify-center items-center rounded" 
 			:class="[ 	status == 2 ?	'bg-yellow-500 text-white' :
@@ -13,18 +7,13 @@
 										'bg-gray-200 ']">
 			{{type}} sandbox {{status_str}}
 		</div>
-		
 		<div class="flex items-stretch">
-			<UiButton class="mr-1 flex justify-center items-center flex-grow" :class="[
-				ui_inspect_sandbox ? 'bg-red-800 border-t-red-900 border-b-red-700 '
-									+'hover:bg-orange-900 hover:border-t-black border-b-red-800' : ''
-			]" @click.stop.prevent="toggleInspect">
+			<UiButton class="mr-1 flex justify-center items-center flex-grow" :pressed="ui_inspect_sandbox" @click.stop.prevent="toggleInspect">
 				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" :class="{'text-cyan-800':!ui_inspect_sandbox, 'text-white': ui_inspect_sandbox}" viewBox="0 0 20 20" fill="currentColor">
 					<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8 7a1 1 0 00-1 1v4a1 1 0 001 1h4a1 1 0 001-1V8a1 1 0 00-1-1H8z" clip-rule="evenodd" />
 				</svg>
 				Inspect
 			</UiButton>
-
 			<UiButton class="flex justify-center items-center flex-grow" type="submit" @click.stop.prevent="stopSandbox()" :disabled="!type">
 				<svg class="w-6 h-6 mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
 					<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
@@ -37,7 +26,6 @@
 
 <script lang="ts">
 import { defineComponent, ref, watch, onBeforeMount, computed } from 'vue';
-import appspaceStatus from '../models/appspace-status';
 import sandboxControl from '../models/sandbox-control';
 
 import UiButton from './ui/UiButton.vue';
@@ -76,7 +64,6 @@ export default defineComponent({
 		})
 
 		return {
-			appspaceStatus,
 			ui_inspect_sandbox,
 			toggleInspect,
 			stopSandbox,
--- frontend-ds-dev/src/components/ui/UiButton.vue
+++ frontend-ds-dev/src/components/ui/UiButton.vue
@@ -1,15 +1,14 @@
 <template>
-	<button class="bg-cyan-600 
-			px-4 py-1 rounded 
-			border-t-2 border-t-cyan-600
-			border-b-2 border-b-cyan-800
+	<button class="	px-4 py-1 rounded border-t-2 border-b-2
 			text-white font-medium text-xs leading-tight uppercase 
-			hover:bg-cyan-700 hover:border-t-cyan-600 hover:border-b-cyan-900
 			focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-cyan-600			
 			active:bg-cyan-800
 			disabled:bg-cyan-200 disabled:border-y-cyan-200 disabled:text-cyan-50
 			transition duration-150 ease-in-out"
-			:class="{'bg-cyan-800':pressed, 'border-t-cyan-900':pressed, 'border-b-cyan-600':pressed}">
+			:class="[ pressed ?  'bg-red-800 border-t-red-900 border-b-red-700 '
+								+'hover:bg-orange-900 hover:border-t-black border-b-red-800' :
+								 'bg-cyan-600  border-t-cyan-600 border-b-cyan-800 '
+								+'hover:bg-cyan-700 hover:border-t-cyan-600 hover:border-b-cyan-900']">
 		<slot></slot>
 	</button>
 </template>
