8900d6e00a64f49decdfeaf22939b02bbb374008
O F <teleclimber@gmail.com>
2022-10-18 16:41:51 -0700

    comments: in support of #88

--- denosandboxcode/services/routeserver.ts
+++ denosandboxcode/services/routeserver.ts
@@ -79,35 +79,43 @@ export default class DsRouteServer {
 		const headers = reqEvent.request.headers;
 		const matched_route = headers.get("X-Dropserver-Route-ID");
 		if( matched_route === null ) {
-			this.replyError(reqEvent, "X-Dropserver-Route-ID header is null");
+			this.replyError(reqEvent, "X-Dropserver-Route-ID header is null");	
+			// How would this happen? No sure, but it's not an app error, 
+			// it's DS that didn't registerroutes correctly, or some other snafu.
 			return;
 		}
 		const route = this.appRoutes.getRouteWithMatch(matched_route);
 		if( route === undefined) {
 			this.replyError(reqEvent, "route id not found");
+			// similar to above
 			return;
 		}
 		if( route.type !== RouteType.function ) {
 			this.replyError(reqEvent, "matched route is not of type function");
+			// same again
 			return;
 		}
 		if( !route.handler ) {
 			this.replyError(reqEvent, "no handler attached to route");
+			// same.
 			return;
 		}
 		if( route.match === undefined ) {
+			// same again
 			throw new Error("route returned without match function");
 		}
 
 		const req_url_str = headers.get("X-Dropserver-Request-URL");
 		if( !req_url_str ) {
 			this.replyError(reqEvent, "no request url found in headers");
+			// That's a DS screw up
 			return;
 		}
 		const req_url = new URL(req_url_str, "https://appspace/");
 		const route_match = route.match(req_url.pathname);
 		if( !route_match ) {
 			this.replyError(reqEvent, "route failed to match in sandbox");
+			// Weird incompatibility between ds host side and sandbox side route matching
 			return;
 		}
 
@@ -125,6 +133,9 @@ export default class DsRouteServer {
 		}
 		catch(e) {
 			this.replyError(reqEvent, e);
+			// this error comes from app code, but it may also be from dropserver_app lib
+			// But that's technically app code. Maybe dropserver_lib_suport can define an error
+			// That then gets used to indicate a problem at the library level?
 			return;
 		}
 
